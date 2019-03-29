39bafe28c937b1d72d46c1298952acb8f5c09a8b
Allan <allanbrados@gmail.com>
2019-03-29 14:01:05 -0300

    remove/comment db configs

--- mix.exs
+++ mix.exs
@@ -35,8 +35,6 @@ defmodule LiveDoomFire.Mixfile do
     [
       {:phoenix, "~> 1.4", override: true},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
@@ -48,17 +46,9 @@ defmodule LiveDoomFire.Mixfile do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["test"]
     ]
   end
 end
